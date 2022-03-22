01ff3bbfff6e98bb312ed9955166d8d6e844a2bc
taku1219 <acmiran189926@gmail.com>
2022-03-22 19:55:00 +0900

    [add] /api/customersにアクセスしたときにカスタマー情報が返るように

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ app.listen(PORT, () => console.log('サーバーが起動しました'));
 app.get('/', (req, res) => {
   res.send('Udemy講座を受講中');
 });
+
+const customers = [
+  { name: '田中', id: 1 },
+  { name: '佐藤', id: 2 },
+  { name: '鈴木', id: 3 },
+  { name: '木村', id: 4 },
+  { name: '高橋', id: 5 },
+];
+
+app.get('/api/customers', (req, res) => {
+  res.send(customers);
+});
